b5fbb484307026a82cf999d88f666cf3204569ce
Deku <zachdsamson@gmail.com>
2021-07-18 12:30:12 -0700

    added persistance

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,8 @@
 var dayStartHour = 9;
 // how many hours the day planner will generate
 var hoursDisplayed = 14;
+// an empty array to store saved and loaded time blocks in
+schedArray = [];
 
 // selects the div to hold all time blocks
 var scheduleTable = $(".container");
@@ -9,6 +11,23 @@ var scheduleTable = $(".container");
 // generate current date and push to header
 var now = moment().format("dddd, MMMM Do");
 var currentDayEl = $("#currentDay").text(now);
+
+function loadTask() {
+    sched = JSON.parse(localStorage.getItem("sched"));
+    if (!sched) {
+        // an empty array to store saved tasks
+        schedArray = [];
+        for(i=0 ; i<hoursDisplayed ; i++) {
+            taskObj = {};
+            taskObj[i] = "";
+            schedArray.push(taskObj);
+        };
+            }
+    else {
+        schedArray = sched;
+    }
+};
+loadTask();
 
 // dynamically generate time blocks
 for(i=0; i<hoursDisplayed; i++ ) {
@@ -25,37 +44,45 @@ for(i=0; i<hoursDisplayed; i++ ) {
     var hourGenerated = parseInt(moment().hour(dayStartHour+i).format("H"));
     var hourDiff = hourCurrent - hourGenerated;
 
+    var textEl = $("<textarea>").addClass("col-10 description textarea");
+    
     if(hourDiff > 0){
-        var textEl = $("<textarea>")
-        .addClass("col-10 description")
-        .addClass("past");
+        textEl.addClass("past");
     }
     else if(hourDiff == 0){
-        var textEl = $("<textarea>")
-        .addClass("col-10 description")
         textEl.addClass("present");
     }
     else {
-        var textEl = $("<textarea>")
-        .addClass("col-10 description")
         textEl.addClass("future");   
     }
 
+    textEl.val(schedArray[i][i]);
+
     var btnEl = $("<button>")
         .addClass("col-1 saveBtn");
+    btnEl.text("Save")
+    
 
     timeBlockEl.append(hourEl, textEl, btnEl);
     scheduleTable.append(timeBlockEl);
 };
 
-// task text was clicked
+// accepts the ID and USER INPUT TEXT of the timeblock whose save button was clicked
+function saveTask(id, text) {
+    var taskObj = {};
+    taskObj[id] = text;
+    schedArray.splice(id,1,taskObj);
+    localStorage.setItem("sched", JSON.stringify(schedArray));
+};
+
+// task save button was clicked
 $(".container").on("click", "button", function() {
     // get the id of the time block who's button was clicked
     var timeBlockSelected = $(this).closest("div.row");
-    console.log(timeBlockSelected);
+    
+    var timeBlockId = $(this).closest("div.row").attr("id");
 
     var enteredText = timeBlockSelected.children(".description")
         .val();
-    console.log(enteredText);
-  
+    saveTask(timeBlockId, enteredText);
 });
