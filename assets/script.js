91b01c271e395add69fe7f051f20596ee747832a
Deku <zachdsamson@gmail.com>
2021-07-18 17:23:49 -0700

    added comments

--- assets/script.js
+++ assets/script.js
@@ -12,6 +12,7 @@ var scheduleTable = $(".container");
 var now = moment().format("dddd, MMMM Do");
 var currentDayEl = $("#currentDay").text(now);
 
+// grabs array of saved (if any) time blocks
 function loadTask() {
     sched = JSON.parse(localStorage.getItem("sched"));
     if (!sched) {
@@ -32,20 +33,25 @@ loadTask();
 // dynamically generate time blocks
 for(i=0; i<hoursDisplayed; i++ ) {
 
+    // div that will hold time blocks
     var timeBlockEl = $("<div>")
         .addClass("row time-block")
         .attr("id", i);
 
+    // time blocks that will hold save-button, textarea, and time
     var hourEl = $("<div>")
         .addClass("col-1 hour")
         .text(moment().hour(dayStartHour+i).format("hA"));
 
+    // grabs current time, generated time block time, and compares the two
     var hourCurrent = moment().hour();
     var hourGenerated = parseInt(moment().hour(dayStartHour+i).format("H"));
     var hourDiff = hourCurrent - hourGenerated;
 
+    // textarea to accept user input
     var textEl = $("<textarea>").addClass("col-10 description textarea");
     
+    // assigns color of text area based on current time
     if(hourDiff > 0){
         textEl.addClass("past");
     }
@@ -56,13 +62,15 @@ for(i=0; i<hoursDisplayed; i++ ) {
         textEl.addClass("future");   
     }
 
+    // assigns text area value from local storage
     textEl.val(schedArray[i][i]);
 
+    // save button 
     var btnEl = $("<button>")
         .addClass("col-1 saveBtn");
     btnEl.text("Save")
     
-
+    // appends all time blocks to the page
     timeBlockEl.append(hourEl, textEl, btnEl);
     scheduleTable.append(timeBlockEl);
 };
